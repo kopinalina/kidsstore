e6cda02e473cb87e1bad631b25a6a1bbf8556acb
kopinalina <bendyuga@gmail.com>
2020-11-03 13:15:22 +0000

    git commit -m

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date())
+    var time = Date.parse(endtime) - Date.parse(new Date())
   return {
-    'total': t,
-    'days': Math.floor(t / (1000 * 60 * 60 * 24)),
-    'hours': Math.floor((t / (1000 * 60 * 60)) % 24),
-    'minutes': Math.floor((t / 1000 / 60) % 60),
-    'seconds': Math.floor((t / 1000) % 60)
+    'total': time,
+    'days': Math.floor(time / (1000 * 60 * 60 * 24)),
+    'hours': Math.floor((time / (1000 * 60 * 60)) % 24),
+    'minutes': Math.floor((time / 1000 / 60) % 60),
+    'seconds': Math.floor((time / 1000) % 60)
   };
 }
  
@@ -17,14 +17,14 @@ function initializeClock(selector, endtime) {
   const seconds = clock.querySelector('.second.number');
  
   function updateClock() {
-    var t = getTimeRemaining(endtime);
+    var time = getTimeRemaining(endtime);
  
-    days.innerHTML = t.days;
-    hours.innerHTML = ('0' + t.hours).slice(-2);
-    minutes.innerHTML = ('0' + t.minutes).slice(-2);
-    seconds.innerHTML = ('0' + t.seconds).slice(-2);
+    days.innerHTML = time.days;
+    hours.innerHTML = ('0' + time.hours).slice(-2);
+    minutes.innerHTML = ('0' + time.minutes).slice(-2);
+    seconds.innerHTML = ('0' + time.seconds).slice(-2);
  
-    if (t.total <= 0) {
+    if (time.total <= 0) {
       clearInterval(timeinterval);
     }
   }
